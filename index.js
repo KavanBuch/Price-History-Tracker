fa5c011fd0066ae1e2f3e61e8e080c384186c400
kavan02 <kavanbuch02@gmail.com>
2022-11-21 03:26:27 +0530

    Users CRUD Done

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const { Client } = require("pg");
-const ejs = require("ejs");
-const bcrypt = require("bcrypt");
-const session = require("express-session");
+const methodOverride = require("method-override");
 
 const client = new Client({
   user: "postgres",
@@ -25,40 +23,80 @@ try {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+//middlewares
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: "topsecretkey" }));
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
+//home page
+app.get("/home", (req, res) => {
   res.render("home");
 });
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.post("/register", async (req, res) => {
-  const { firstName, lastName, userName, email, contactNumber, dob, password } =
-    req.body;
-  res.send(req.body);
-});
-
-app.get("/category", async (req, res) => {
-  const category = await client.query("select * from pht_db.category");
-  res.render("category", { category: category.rows });
-});
-
-app.get("/category/:id", async (req, res) => {
-  const { id } = req.params;
-  const queryText = `select * from (select * from pht_db.product_details inner join pht_db.category on product_details.ce_id=category.category_id) as details where details.ce_id=${id}`;
-  const category = await client.query(queryText);
-  res.render("specificCategory", { category: category.rows });
-});
-
-app.get("/product/:id", async (req, res) => {
-  const { id } = req.params;
-  const queryText = `select * from (select * from pht_db.product_details inner join pht_db.product_ratings on product_id=p_id inner join pht_db.e_commerce_websites on e_commerce_websites.e_id=web_id) as details where details.p_id=${id}`;
+//end users CRUD
+app.get("/endUsers", async (req, res) => {
+  const queryText = `select * from pht_db.end_user`;
   const { rows } = await client.query(queryText);
-  res.render("product", { product: rows });
+  res.render("endUsers", { users: rows });
+});
+
+app.get("/endUsers/addUser", async (req, res) => {
+  res.render("addUser");
+});
+
+app.post("/endUsers/addUser", async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    user_name,
+    email,
+    contact_number,
+    date_of_birth,
+    password,
+  } = req.body;
+  const { rows } = await client.query(
+    `select max(user_id) from pht_db.end_user`
+  );
+  const id = Number(rows[0].max) + 1;
+  const queryText = `insert into pht_db.end_user(first_name,last_name,user_name,email,contact_number,date_of_birth,password,user_role,user_id) values('${first_name}','${last_name}','${user_name}','${email}','${contact_number}','${date_of_birth}','${password}','end_user',${id})`;
+  await client.query(queryText);
+  res.render("addUserResult");
+});
+
+app.get("/endUsers/editUser/:id", async (req, res) => {
+  const { id } = req.params;
+  const queryText = `select * from pht_db.end_user where user_id=${id}`;
+  const { rows } = await client.query(queryText);
+  res.render("editUser", { user: rows[0] });
+});
+
+app.patch("/endUsers/editUser/:id", async (req, res) => {
+  const { id } = req.params;
+  const {
+    first_name,
+    last_name,
+    user_name,
+    email,
+    contact_number,
+    date_of_birth,
+    password,
+  } = req.body;
+  const queryText = `update pht_db.end_user set first_name='${first_name}',last_name='${last_name}',user_name='${user_name}',email='${email}',contact_number='${contact_number}',date_of_birth='${date_of_birth}',password='${password}' where user_id=${id}`;
+  await client.query(queryText);
+  res.render("editUserResult");
+});
+
+app.delete("/endUsers/deleteUser/:id", async (req, res) => {
+  const { id } = req.params;
+  const queryText = `delete from pht_db.end_user where user_id=${id}`;
+  await client.query(queryText);
+  res.redirect("/endUsers");
+});
+
+//products CRUD
+app.get("/products", async (req, res) => {
+  const queryText = `select * from pht_db.product_details`;
+  const { rows } = await client.query(queryText);
+  res.render("products", { products: rows });
 });
 
 app.listen(80, () => {
